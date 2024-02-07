66364a5784e30f1f957925fbd34742056acbe086
gustavohpereira <gustavohpa2003@gmail.com>
2024-02-07 08:18:50 -0300

    fix format

--- portfolio-gustavo/src/data/SliderData.ts
+++ portfolio-gustavo/src/data/SliderData.ts
@@ -1,32 +1,36 @@
 const SliderData = [
-    {
-      source: "/API1.gif",
-      name: "Assistência Técnica Escolar",
-      description: "o projeto SOS FATEC foi desenvolvido para facilitar a organização da assistência técnica em ambientes escolares. A aplicação analisa o estado das máquinas, notifica o técnico responsável sobre problemas identificados e permite a criação de chamados. Além disso, proporciona ao técnico a capacidade de reorganizar os computadores em salas, por meio de drag and drop, gerando um mapa para facilitar o suporte.",
-      technologies: "Html,Css, Javascript, Flask, python, MySQL, Bootstrap",
-      gitLink: "https://github.com/gustavohpereira/API_MirageGroup"
-    },
-    {
-      source: "/API2.gif",
-      name: "Gerenciamento de Turmas",
-      description: "Desenvolvido em parceria com uma professora o projeto API2, uma aplicação Java para organizar aulas, alunos, avaliações, notas e atividades. Este aplicativo opera totalmente offline, proporcionando flexibilidade e praticidade para a gestão acadêmica, mesmo em ambientes sem acesso à internet.",
-      technologies: "Java, JavaFX, SQLite",
-      gitLink: "https://github.com/MirageGroup/API_MirageGroup_2sem"
-    },
-    {
-      source: "/API3.png",
-      name: "Gerenciamento de Projetos - Shorts",
-      description: "Em colaboração com a Ionic Health, desenvolvi o projeto Shorts, uma aplicação React TypeScript com Node.js. Esta ferramenta permite a organização de equipes e projetos, oferecendo recursos para armazenar evidências, gerenciar pessoas e manter um fluxo eficiente de trabalho. Inclui uma funcionalidade de organização kanban para tarefas.",
-      technologies: "TypeScript, React, Node.js, TypeORM, SQL",
-      gitLink: "https://github.com/MirageGroup/API_MirageGroup_3sem"
-    },
-    {
-        source: "/Portfolio.png",
-        name: "Portfólio",
-        description: "Este projeto foi desenvolvido para mostrar um pouco do meu trabalho e me apresentar. O site foi desenvolvido utilizando Next, Typescript e Tailwind CSS.",
-        technologies: "TypeScript, Next, Tailwind",
-        gitLink: "https://github.com/gustavohpereira/portfolio"
-      }
-  ];
+  {
+    source: "/API1.gif",
+    name: "Assistência Técnica Escolar",
+    description:
+      "o projeto SOS FATEC foi desenvolvido para facilitar a organização da assistência técnica em ambientes escolares. A aplicação analisa o estado das máquinas, notifica o técnico responsável sobre problemas identificados e permite a criação de chamados. Além disso, proporciona ao técnico a capacidade de reorganizar os computadores em salas, por meio de drag and drop, gerando um mapa para facilitar o suporte.",
+    technologies: "Html,Css, Javascript, Flask, python, MySQL, Bootstrap",
+    gitLink: "https://github.com/gustavohpereira/API_MirageGroup",
+  },
+  {
+    source: "/API2.gif",
+    name: "Gerenciamento de Turmas",
+    description:
+      "Desenvolvido em parceria com uma professora o projeto API2, uma aplicação Java para organizar aulas, alunos, avaliações, notas e atividades. Este aplicativo opera totalmente offline, proporcionando flexibilidade e praticidade para a gestão acadêmica, mesmo em ambientes sem acesso à internet.",
+    technologies: "Java, JavaFX, SQLite",
+    gitLink: "https://github.com/MirageGroup/API_MirageGroup_2sem",
+  },
+  {
+    source: "/API3.png",
+    name: "Gerenciamento de Projetos - Shorts",
+    description:
+      "Em colaboração com a Ionic Health, desenvolvi o projeto Shorts, uma aplicação React TypeScript com Node.js. Esta ferramenta permite a organização de equipes e projetos, oferecendo recursos para armazenar evidências, gerenciar pessoas e manter um fluxo eficiente de trabalho. Inclui uma funcionalidade de organização kanban para tarefas.",
+    technologies: "TypeScript, React, Node.js, TypeORM, SQL",
+    gitLink: "https://github.com/MirageGroup/API_MirageGroup_3sem",
+  },
+  {
+    source: "/Portfolio.png",
+    name: "Portfólio",
+    description:
+      "Este projeto foi desenvolvido para mostrar um pouco do meu trabalho e me apresentar. O site foi desenvolvido utilizando Next, Typescript e Tailwind CSS.",
+    technologies: "TypeScript, Next, Tailwind",
+    gitLink: "https://github.com/gustavohpereira/portfolio",
+  },
+];
 
-  export default SliderData
+export default SliderData;
